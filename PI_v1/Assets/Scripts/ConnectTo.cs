1e2f11fc7e01493ca54784a516c875b1064eb5bf
AndreiaPp <andreia.portela@ua.pt>
2022-04-28 22:35:33 +0100

    menu 2d done

---
+++ PI_v1/Assets/Scripts/ConnectTo.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConnectTo : MonoBehaviour
+{
+    
+    private string input;
+    public void ReadInput(string ip){
+	input=ip;
+ 	Debug.Log(input);
+	//ligar ao endereço ip passado
+}
+
+    
+}
